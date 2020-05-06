58c1c030a943ea236ba5e77a9f098f488952d920
KaptainWutax <kaptainwutax@gmail.com>
2020-05-06 12:43:42 -0400

    fixed bug

--- src/main/java/kaptainwutax/reverser/Rotation.java
+++ src/main/java/kaptainwutax/reverser/Rotation.java
@@ -5,8 +5,7 @@ import kaptainwutax.seedutils.mc.MCVersion;
 import kaptainwutax.seedutils.util.Mth;
 
 public class Rotation {
-
-	private static final int MASK_31 = (int)Mth.mask(31);
+	
 	private static final LCG SKIP = LCG.JAVA.combine(2);
 
 	public static int getModel(int posX, int posY, int posZ, int totalWeight, MCVersion version) {
@@ -15,7 +14,7 @@ public class Rotation {
 		if(version.isOlderThan(MCVersion.v1_13)) {
 			return Math.abs((int)seed) % totalWeight;
 		} else {
-			return (int)(SKIP.nextSeed(seed ^ LCG.JAVA.multiplier) >> 16 & MASK_31) % totalWeight;
+			return Math.abs((int)(SKIP.nextSeed(seed ^ LCG.JAVA.multiplier) >> 16)) % totalWeight;
 		}
 	}
 
@@ -23,6 +22,12 @@ public class Rotation {
 		long seed = (long)(posX * 3129871) ^ (long)posZ * 116129781L ^ (long)posY;
 		seed = seed * seed * 42317861L + seed * 11L;
 		return seed >> 16;
+	}
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 500; i++) {
+			System.out.println(getModel(i, 0, 0, 16, MCVersion.v1_12));
+		}
 	}
 
 }
